edb5615137bb184d4caeaf4e9893c17c401bbeaf
mvaradi <mvaradi@ebi.ac.uk>
2021-03-22 16:22:10 +0000

    Adding more tests

--- src/app/summary-icon/summary-icon.component.spec.ts
+++ src/app/summary-icon/summary-icon.component.spec.ts
@@ -28,9 +28,48 @@ describe('SummaryIconComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  // it('getSuperposeData() should work', () => {
-  //   component.accession = 'foo';
-  //   expect(component.getSuperposeData()).toEqual({accession: 'foo', segment: 1});
-  // });
+  it('getSuperposeData() should work', () => {
+    component.accession = 'foo';
+    const expected = {
+      'accession': 'foo',
+      'superposition': true,
+      'superpositionParams': {
+        'segment': 1
+      }
+    };
+    expect(component.getSuperposeData()).toEqual(expected);
+  });
+
+  it('getSuperposeData() should work with ligand view', () => {
+    component.accession = 'foo';
+    const expected = {
+      'accession': 'foo',
+      'superposition': true,
+      'superpositionParams': {
+        'segment': 1,
+        'ligandView': true,
+        'superposeAll': true
+      }
+    };
+    expect(component.getSuperposeData(true)).toEqual(expected);
+  });
+
+  it('getSuperposeData() should work with parent accession', () => {
+    component.accession = 'foo';
+    component.parentAccession = 'bar';
+    const expected = {
+      'accession': 'foo',
+      'superposition': true,
+      'superpositionParams': {
+        'segment': 1,
+        'matrixAccession': 'bar'
+      }
+    };
+    expect(component.getSuperposeData()).toEqual(expected);
+  });
+
+  it('scrollToId() should not fail', () => {
+    expect(component.scrollToId('main')).toBeFalsy();
+  });
 
 });
